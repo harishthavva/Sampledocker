49b4f80e521a14b072aa12f13060cae6a9a59472
thavvaharish <harish.thavva@gmail.com>
2021-11-22 10:52:10 +0530

    creating REACT app

---
+++ src/Family.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Family extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello My name is Harish <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/64/000000/external-Emoji-valentine-love-bearicons-outline-color-bearicons.png"/></h1>
+                <h3>I am from Ongole</h3>
+            </div>
+        );
+    }
+}
+
+export default Family;
