a98e2ab39ead8f76aa4fbf74c65e96a35d8d9913
Harish <69967042+harishthavva@users.noreply.github.com>
2021-11-22 15:52:28 +0530

    Removing Emoji

--- src/Family.js
+++ src/Family.js
@@ -4,7 +4,7 @@ class Family extends Component {
     render() {
         return (
             <div>
-                <h1>Hello My name is Harish <img src="https://img.icons8.com/external-bearicons-outline-color-bearicons/64/000000/external-Emoji-valentine-love-bearicons-outline-color-bearicons.png"/></h1>
+                <h1>Hello My name is Harish</h1>
                 <h3>I am from Ongole</h3>
             </div>
         );
